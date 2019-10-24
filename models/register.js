18a2b8a54ac41b5398b68186c49430a2d08059f4
David <blittynet@gmail.com>
2019-10-24 20:49:47 +0200

    changed phone to String
    update

--- models/register.js
+++ models/register.js
@@ -6,7 +6,7 @@ const registerSchema = new Schema({
     lastName: { type: String, required: [true, 'Last name required'] }, 
     mail: { type: String, unique: true, required: [true, 'Mail required'] },
     password: { type: String, required: [true, 'Password required'] },
-    phone: { type: Number, min: 600000000, max: 777777777 },
+    phone: { type: String, minlength: 9, maxlength: 9 },
     date: { type: Date, default: Date.now() }
 })
 
